2af69e20baa572338cebd2a80ac3c9ae591951d7
BoyJirayu <beboy_2538@hotmail.com>
2021-03-01 10:00:54 +0700

    hello world

--- test.py
+++ test.py
@@ -1,47 +1,50 @@
-# from flask import Flask
-# app = Flask(__name__)
-# @app.route('/')
-# def MainFunction():
-#     return 'Hello world'
-# if __name__ == '__main__':
-#     app.run()
-
-from flask import Flask, request
-import json
-import requests
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-def index():
-  return "Hello World!"
-
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
-def callback():
-  json_line = request.get_json()
-  json_line = json.dumps(json_line)
-  decoded = json.loads(json_line)
-  user = decoded["events"][0]['replyToken']
-  #id=[d['replyToken'] for d in user][0]
-  #print(json_line)
-  print("ผู้ใช้：",user)
-  sendText(user,'งง') # ส่งข้อความ งง
-  return '',200
-
-
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  Authorization = 'CxoEeTecHEIzApUgMPohAgQALqbSi/i0xq0Qx16YGDjwyqSXUXbtbul7oA8t1EiF6Ti+ylCs3JRmMLhBAyWP3l8r8KoCrPymAt4F+6j0AqO5Dg7ArwsY0YRgN+trYbjUyvbPqywXerDtCBGIbOAd/AdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
-  headers = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Authorization':Authorization
-  }
-  data = json.dumps({
-  "replyToken":user,
-  "messages":[{"type":"text","text":text}]})
-  #print("ข้อมูล：",data)
-  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-  #print(r.text)
-
-
+def MainFunction():
+    return 'Hello world'
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run()
+
+# from flask import Flask, request, abort
+# import requests
+# import json
+# app = Flask(__name__)
+# @app.route('/', methods=['POST','GET'])
+
+# def webhook():
+#   if request.method == 'POST':
+#     payload = request.json
+#     Reply_token = payload['events'][0]['replyToken']
+#     print(Reply_token)
+#     message = payload['events'][0]['message']['text']
+#     print(message)
+#     if 'ดี' in message :
+#       Reply_messasge = 'ดีมาก'
+#       ReplyMessage(Reply_token,Reply_messasge,'CxoEeTecHEIzApUgMPohAgQALqbSi/i0xq0Qx16YGDjwyqSXUXbtbul7oA8t1EiF6Ti+ylCs3JRmMLhBAyWP3l8r8KoCrPymAt4F+6j0AqO5Dg7ArwsY0YRgN+trYbjUyvbPqywXerDtCBGIbOAd/AdB04t89/1O/w1cDnyilFU=') #ใส่ Channel access token
+#       return request.json, 200
+#   else:
+#     abort(400)
+
+# def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
+#   LINE_API = 'https://api.line.me/v2/bot/message/reply'
+
+#   Authorization = 'Bearer {}'.format(Line_Acees_Token)
+#   print(Authorization)
+#   headers = {
+#     'Content-Type': 'application/json; charset=UTF-8',
+#     'Authorization':Authorization
+#   }
+#   data = {
+#     "replyToken":Reply_token,
+#     "messages":[{
+#       "type":"text",
+#       "text":TextMessage
+#     }]
+#   }
+#   data = json.dumps(data)
+#   r = requests.post(LINE_API, headers=headers, data=data)
+#   return 200
+
+# if __name__ == '__main__':
+#   app.run(debug=True)
