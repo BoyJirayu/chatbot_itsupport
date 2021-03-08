0e2554c9a916da1f2aa2d6fb20dc9f1a49f04e11
BoyJirayu <beboy_2538@hotmail.com>
2021-03-08 15:34:09 +0700

    Edit to Firebase Realtime Database

--- connect_firebase.py
+++ connect_firebase.py
@@ -1,20 +1,35 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-cred = credentials.Certificate('line-itsupport-firebase-firebase-adminsdk-fvrmd-69aedaa3b9.json')
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+from firebase import firebase
 
-# # Write Data to Firebase
-# doc_ref = db.collection(u'users').document(u'Name')
-# doc_ref.set({
-#     u'first': u'Jirayu',
-#     u'last': u'Chaimeeboon',
-# })
+url = 'https://line-itsupport-firebase-default-rtdb.firebaseio.com/'
+messenger = firebase.FirebaseApplication(url)
 
-# # Read Data from Firebase
-# users_ref = db.collection(u'users')
-# docs = users_ref.stream()
+engineer = {'id':1001,'name':'Uncle Engineer'}
+engineer2 = {'id':1002,'name':'Lung Tu'}
 
-# for doc in docs:
-#     print(u'{} => {}'.format(doc.id, doc.to_dict()))
+result = messenger.put('/user','1',engineer)
+result2 = messenger.put('/user','2',engineer2)
+
+print("Engineer 1", result)
+print("Engineer 2", result2)
+
+
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# cred = credentials.Certificate('line-itsupport-firebase-firebase-adminsdk-fvrmd-69aedaa3b9.json')
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+# # # Write Data to Firebase
+# # doc_ref = db.collection(u'users').document(u'Name')
+# # doc_ref.set({
+# #     u'first': u'Jirayu',
+# #     u'last': u'Chaimeeboon',
+# # })
+
+# # # Read Data from Firebase
+# # users_ref = db.collection(u'users')
+# # docs = users_ref.stream()
+
+# # for doc in docs:
+# #     print(u'{} => {}'.format(doc.id, doc.to_dict()))
