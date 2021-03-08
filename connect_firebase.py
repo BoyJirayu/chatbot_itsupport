8f242f8a5eeda21277351889c3efda6235be622b
BoyJirayu <beboy_2538@hotmail.com>
2021-03-08 09:21:39 +0700

    connect Firebase

---
+++ connect_firebase.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate('line-itsupport-firebase-firebase-adminsdk-fvrmd-69aedaa3b9.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+## Write Data to Firebase
+# doc_ref = db.collection(u'users').document(u'aturing')
+# doc_ref.set({
+#     u'first': u'Alan',
+#     u'last': u'Turing',
+# })
+
+## Read Data from Firebase
+# users_ref = db.collection(u'users')
+# docs = users_ref.stream()
+
+# for doc in docs:
+#     print(u'{} => {}'.format(doc.id, doc.to_dict()))
