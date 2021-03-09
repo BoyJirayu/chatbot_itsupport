3310840da87b12625e5fd8bd0d61ef75e48c1d76
BoyJirayu <beboy_2538@hotmail.com>
2021-03-09 22:00:05 +0700

    push profile user to database complete

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify, render_template, request
 from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
 from linebot.models.template import *
 from linebot import LineBotApi, WebhookHandler
-# from use_model import myPredict, lamda_l
-# from firebase import firebase
-# from connect_firebase import *
+from use_model import myPredict, lamda_l
+from firebase import firebase
+from connect_firebase import *
 
 import json
 import numpy as np
+
+firebase = firebase.FirebaseApplication('https://line-itsupport-firebase-default-rtdb.firebaseio.com/', None)
 
 app = Flask(__name__)
 
@@ -43,47 +45,50 @@ def event_handle(event):
     profile = line_bot_api.get_profile(userID)
     user_name = profile.display_name
     user_pic = profile.picture_url
+    db_name = "User Profile"
+    data = {"Display_Name": user_name, "Profile_Pic": user_pic}
+
+    post_user_pofile(uid=userID,data=data,firebase_app=firebase,db_name=db_name)
 
     if msgType == "text":
       message = str(event["message"]["text"])
 
-      Reply_message = "สวัสดีครับ K'" + user_name +"\n" + " IT Support ยินดีให้บริการครับ"
-      # if message :
-      #   Label_message = myPredict(message)
-      #   if Label_message == "ทักทาย" :
-      #     Reply_message = "สวัสดีครับ K'" + user_name +"\n" + " IT Support ยินดีให้บริการครับ"
-      #   elif Label_message == "เสร็จสิ้นและขอบคุณ" :
-      #     Reply_message = "IT Support ยินดีให้บริการครับ ขอบคุณครับ"
-      #   elif Label_message == "Video Conference" :
-      #     Reply_message = "ถ้าท่านต้องการใช้งานระบบ Video Conference"+"\n"+"โปรดแจ้งรายละเอียด ดังนี้"+"\n"+"ชื่อการประชุม วันที่ เวลา และต้องการอุปกรณ์สำหรับประชุม Video Conference หรือไม่"
-      #   elif Label_message == "ระบบ E-Budget" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "ระบบ VPN" :
-      #     Reply_message = "ท่านสามารถใช้งานผ่าน Web Browser ได้ที่ลิงก์ https://vpn.mhesi.go.th/dana-na/auth/url_3/welcome.cgi"
-      #   elif Label_message == "ระบบ ERP" :
-      #     Reply_message = "สามารถใช้ฃานได้ผ่าน https://erp.mhesi.go.th/"
-      #   elif Label_message == "Share Drive" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "Back Office System" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "Email" :
-      #     Reply_message = "สามารถใช้งานได้ที่ webmail.mhesi.go.th"
-      #   elif Label_message == "Internet" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "Network Security" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "เครื่องแม่ข่าย VM & Server" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "Printer & Scanner" :
-      #     Reply_message = "รับทราบครับ เดี๋ยวเจ้าหน้าที่จะดำเนินการติดตั้งหรือแก้ปัญหาเครื่อง Printer/Scanner ของท่านให้ครับ"
-      #   elif Label_message == "ปัญหา Computer" :
-      #     Reply_message = "Test"
-      #   elif Label_message == "ยืมคืน Accessory" :
-      #     Reply_message = "Test"
+      if message :
+        Label_message = myPredict(message)
+        if Label_message == "ทักทาย" :
+          Reply_message = "สวัสดีครับ K'" + user_name +"\n" + " IT Support ยินดีให้บริการครับ"
+        elif Label_message == "เสร็จสิ้นและขอบคุณ" :
+          Reply_message = "IT Support ยินดีให้บริการครับ ขอบคุณครับ"
+        elif Label_message == "Video Conference" :
+          Reply_message = "ถ้าท่านต้องการใช้งานระบบ Video Conference"+"\n"+"โปรดแจ้งรายละเอียด ดังนี้"+"\n"+"ชื่อการประชุม วันที่ เวลา และต้องการอุปกรณ์สำหรับประชุม Video Conference หรือไม่"
+        elif Label_message == "ระบบ E-Budget" :
+          Reply_message = "Test"
+        elif Label_message == "ระบบ VPN" :
+          Reply_message = "ท่านสามารถใช้งานผ่าน Web Browser ได้ที่ลิงก์ https://vpn.mhesi.go.th/dana-na/auth/url_3/welcome.cgi"
+        elif Label_message == "ระบบ ERP" :
+          Reply_message = "สามารถใช้ฃานได้ผ่าน https://erp.mhesi.go.th/"
+        elif Label_message == "Share Drive" :
+          Reply_message = "Test"
+        elif Label_message == "Back Office System" :
+          Reply_message = "Test"
+        elif Label_message == "Email" :
+          Reply_message = "สามารถใช้งานได้ที่ webmail.mhesi.go.th"
+        elif Label_message == "Internet" :
+          Reply_message = "Test"
+        elif Label_message == "Network Security" :
+          Reply_message = "Test"
+        elif Label_message == "เครื่องแม่ข่าย VM & Server" :
+          Reply_message = "Test"
+        elif Label_message == "Printer & Scanner" :
+          Reply_message = "รับทราบครับ เดี๋ยวเจ้าหน้าที่จะดำเนินการติดตั้งหรือแก้ปัญหาเครื่อง Printer/Scanner ของท่านให้ครับ"
+        elif Label_message == "ปัญหา Computer" :
+          Reply_message = "Test"
+        elif Label_message == "ยืมคืน Accessory" :
+          Reply_message = "Test"
 
-      TextMessage = TextSendMessage(text=Reply_message)
-      Reply_object = TextMessage
-      line_bot_api.reply_message(Reply_token, Reply_object)
+        TextMessage = TextSendMessage(text=Reply_message)
+        Reply_object = TextMessage
+        line_bot_api.reply_message(Reply_token, Reply_object)
 
     else:
         Reply_object = StickerSendMessage(package_id=str(1),sticker_id=str(2))
--- connect_firebase.py
+++ connect_firebase.py
@@ -3,11 +3,15 @@ from firebase import firebase
 url = 'https://line-itsupport-firebase-default-rtdb.firebaseio.com/'
 messenger = firebase.FirebaseApplication(url)
 
-engineer = {'id':1001,'name':'Uncle Engineer'}
-engineer2 = {'id':1002,'name':'Lung Tu'}
+def post_user_pofile(uid,data,firebase_app,db_name):
+    res = firebase_app.patch("/"+db_name+"/"+uid,data)
+    return res
 
-result = messenger.put('/user','1',engineer)
-result2 = messenger.put('/user','2',engineer2)
+# engineer = {'id':1001,'name':'Uncle Engineer'}
+# engineer2 = {'id':1002,'name':'Lung Tu'}
 
-print("Engineer 1", result)
-print("Engineer 2", result2)
+# result = messenger.put('/user','1',engineer)
+# result2 = messenger.put('/user','2',engineer2)
+
+# print("Engineer 1", result)
+# print("Engineer 2", result2)
